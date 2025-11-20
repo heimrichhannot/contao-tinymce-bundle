5add3ea1a48f113de94fcd2c1a79846c6343a37e
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-11-20 14:31:44 +0100

    update template, add translations

---
+++ contao/languages/de/tl_form_field.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['useTinymce'] = ['TinyMCE verwenden', 'TinyMCE-Editor für dieses Textfeld verwenden.'];
+$GLOBALS['TL_LANG']['tl_form_field']['tinymceTpl'] = ['TinyMCE-Template', 'Wählen Sie ein Template für die TinyMCE-Editor-Konfiguration.'];
---
+++ contao/languages/en/tl_form_field.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['useTinymce'] = ['Use TinyMCE', 'Use the TinyMCE editor for this textarea field.'];
+$GLOBALS['TL_LANG']['tl_form_field']['tinymceTpl'] = ['TinyMCE template', 'Select a template for the TinyMCE editor configuration.'];
